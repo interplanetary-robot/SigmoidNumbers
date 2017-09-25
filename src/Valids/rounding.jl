5f8cde7c5faf3aa11bf18cb3eeea1325c29c8a2f
Isaac <isaac.yonemoto@gmail.com>
2017-09-25 03:55:09 -0700

    streamlined rounding procedure 	modified:   src/Valids/rounding.jl

--- src/Valids/rounding.jl
+++ src/Valids/rounding.jl
@@ -1,6 +1,6 @@
 
 #unify the three modes because we'll use the same mode for all of them.
-RoundedSigmoid{N,ES} = Union{Sigmoid{N,ES,:upper}, Sigmoid{N,ES,:lower}, Sigmoid{N,ES,:exact}, Sigmoid{N,ES,:cross}, Sigmoid{N,ES,:outward_exact}, Sigmoid{N,ES,:inward_exact}}
+RoundedSigmoid{N,ES} = Union{Sigmoid{N,ES,:upper}, Sigmoid{N,ES,:lower}, Sigmoid{N,ES,:exact}, Sigmoid{N,ES,:outward_exact}, Sigmoid{N,ES,:inward_exact}}
 Direct_Sigmoid{N,ES} = Union{Sigmoid{N,ES,:outward_ulp}, Sigmoid{N,ES,:inward_ulp}}
 
 @generated function __round{N, ES}(x::Union{RoundedSigmoid{N, ES}, Direct_Sigmoid{N,ES}}, extrabits::(@UInt) = zero(@UInt))
@@ -41,57 +41,27 @@ resolve_rounding{N,ES}(x::Sigmoid{N,ES,:exact})         = reinterpret(Sigmoid{N,
 resolve_rounding{N,ES}(x::Sigmoid{N,ES,:outward_exact}) = reinterpret(Sigmoid{N,ES,:ubit}, x)
 resolve_rounding{N,ES}(x::Sigmoid{N,ES,:inward_exact})  = reinterpret(Sigmoid{N,ES,:ubit}, x)
 
-#special resolve_roundings which require an extra specification.
-function resolve_rounding{N,ES}(x::Sigmoid{N,ES,:outward_ulp}, ::Type{Val{:upper}})
-    if !isfinite(x)
-        throw(ArgumentError("an outward ulp cannot be infinite"))
-    elseif Base.iszero(x)
-        maxneg(Vnum{N,ES})
-    elseif x < zero(x)
-        lower_ulp(reinterpret(Vnum{N,ES},x))
-    else
-        upper_ulp(reinterpret(Vnum{N,ES},x))
-    end
-end
-function resolve_rounding{N,ES}(x::Sigmoid{N,ES,:outward_ulp}, ::Type{Val{:lower}})
-    if !isfinite(x)
-        throw(ArgumentError("an outward ulp cannot be infinite"))
-    elseif Base.iszero(x)
-        minpos(Vnum{N,ES})
-    elseif x < zero(x)
-        lower_ulp(reinterpret(Vnum{N,ES},x))
-    else
-        upper_ulp(reinterpret(Vnum{N,ES},x))
-    end
-end
-function resolve_rounding{N,ES}(x::Sigmoid{N,ES,:inward_ulp}, ::Type{Val{:upper}})
-    if !isfinite(x)
-        maxpos(Vnum{N,ES})
-    elseif Base.iszero(x)
-        throw(ArgumentError("an inward ulp cannot be zero"))
-    elseif x < zero(x)
-        upper_ulp(reinterpret(Vnum{N,ES},x))
-    else
-        inner_ulp(reinterpret(Vnum{N,ES},x))
-    end
-end
-function resolve_rounding{N,ES}(x::Sigmoid{N,ES,:inward_ulp}, ::Type{Val{:lower}})
-    if !isfinite(x)
-        minneg(Vnum{N,ES})
-    elseif Base.iszero(x)
-        throw(ArgumentError("an inward ulp cannot be zero"))
-    elseif x < zero(x)
-        upper_ulp(reinterpret(Vnum{N,ES},x))
-    else
-        inner_ulp(reinterpret(Vnum{N,ES},x))
+#special resolve_rounding functions which also must take location into account for the inf/zero values.
+@generated function resolve_rounding{N,ES,mode,location}(x::Sigmoid{N,ES,mode}, ::Type{Val{location}})
+    inf_ulp  = (mode == :outward_ulp) ? :(throw(ArgumentError("an outward ulp cannot be inf"))) : (location == :upper ? :maxpos : :minneg)
+    zero_ulp = (mode == :inward_ulp)  ? :(throw(ArgumentError("an inward ulp cannot be zero"))) : (location == :upper ? :maxneg : :minpos)
+    negative_ulp = (mode == :outward_ulp) ? :lower_ulp : :upper_ulp
+    positive_ulp = (mode == :outward_ulp) ? :upper_ulp : :lower_ulp
+    quote
+        if !isfinite(x)
+            $(inf_ulp)(Vnum{N,ES})
+        elseif Base.iszero(x)
+            $(zero_ulp)(Vnum{N,ES})
+        elseif x < zero(x)
+            $(negative_ulp)(reinterpret(Vnum{N,ES},x))
+        else
+            $(positive_ulp)(reinterpret(Vnum{N,ES},x))
+        end
     end
 end
 
+
+→{N,ES}(lower::RoundedSigmoid{N,ES}, upper::RoundedSigmoid{N,ES}) = resolve_rounding(lower)              → resolve_rounding(upper)
 →{N,ES}(lower::RoundedSigmoid{N,ES}, upper::Direct_Sigmoid{N,ES}) = resolve_rounding(lower)              → resolve_rounding(upper, Val{:upper})
 →{N,ES}(lower::Direct_Sigmoid{N,ES}, upper::RoundedSigmoid{N,ES}) = resolve_rounding(lower, Val{:lower}) → resolve_rounding(upper)
 →{N,ES}(lower::Direct_Sigmoid{N,ES}, upper::Direct_Sigmoid{N,ES}) = resolve_rounding(lower, Val{:lower}) → resolve_rounding(upper, Val{:upper})
-
-→{N,ES}(lower::RoundedSigmoid{N,ES}, upper::RoundedSigmoid{N,ES}) = resolve_rounding(lower)                 → resolve_rounding(upper)
-→{N,ES}(lower::Sigmoid{N,ES,:cross}, upper::RoundedSigmoid{N,ES}) = reinterpret(Sigmoid{N,ES,:lower},lower) → upper
-→{N,ES}(lower::RoundedSigmoid{N,ES}, upper::Sigmoid{N,ES,:cross}) = lower                                   → reinterpret(Sigmoid{N,ES,:upper},upper)
-→{N,ES}(lower::Sigmoid{N,ES,:cross}, upper::Sigmoid{N,ES,:cross}) = reinterpret(Sigmoid{N,ES,:lower},lower) → reinterpret(Sigmoid{N,ES,:upper},upper)
